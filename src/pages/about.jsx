0df1d4492c7f28c06789e6c26d49711b96e04ddc
Fabio Lillman <fabio.lillman@iths.se>
2023-01-20 20:41:05 +0100

    Side animation added to about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,10 +4,44 @@ import SideMenu from "../components/sidemenu"
 import { graphql } from "gatsby"
 // Used to display rich text to correct format
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-// This is a comment to test
 const AboutPage = ({data}) => {
     return (
         <StyledSideMenu>
+          
+          <div className="tube-upper"></div>
+          <div className="connector"></div>
+          <div className="drop-tube">
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="dropplets"></div>
+          <div className="tube-goo top"></div>
+          <div className="tube-goo bot"></div>
+          </div>
+          <svg>
+            <defs>
+              <filter id='goo'>
+                <feGaussianBlur in='SourceGraphic'
+                stdDeviation='10'/>
+                <feColorMatrix in='name' mode='matrix'
+                values='
+                 1 0 0 0 0
+                 0 1 0 0 0 
+                 0 0 1 0 0
+                 0 0 0 30 -25'/>
+
+                 <feBlend in="SourceGraphic"/>
+
+              </filter>
+            </defs>
+          </svg>
+
             <SideMenu/>
             {data.allContentfulAbout.nodes.map(node => (
   <article key={node.id}>  
@@ -49,7 +83,9 @@ const AboutPage = ({data}) => {
 
 
 const StyledSideMenu = styled.main`
+/* Check padding/box-sizing for when releasing the graded version */
 padding: 2rem;
+box-sizing: border-box;
 font-size: 1.3rem;
 height: 100%;
 margin-left: 13rem;
@@ -57,6 +93,150 @@ margin-left: 13rem;
 flex-direction: column;
 justify-content: center;
 align-items: center; 
+overflow: hidden;
+overflow-y: hidden;
+
+/* TUBE ANIMATION */
+        .tube-upper{
+          position: absolute;
+          top: 0;
+          right: 16vmin;
+          width: 8vmin;
+          height: 23vmin;
+
+ background: linear-gradient(30deg, rgba(0,0,0,1) 9%, rgba(0,0,0,0.0) 49%, rgba(0,0,0,1) 80%);
+        border-bottom-left-radius: 30px;
+        &:after{
+          content: "";
+          width: 6vmin;
+          height: 8vmin;
+          background: linear-gradient(217deg, rgba(0,0,0,1) 9%, rgba(0,0,0,0.4290091036414566) 49%, rgba(0,0,0,1) 80%);
+          position: absolute;
+          bottom: 0;
+          right: -6vmin;
+        }
+        }
+        .drop-tube{
+          z-index: -1;
+          overflow: hidden;
+          position: absolute;
+          filter: url('#goo');
+          top: 23vmin;
+          right: 0;
+          width: 8vmin;
+          height: 77vmin;
+          height: calc(100% - 23vmin);
+          background: linear-gradient(90deg, rgba(133,224,139,1) 0%, rgba(0,255,132,0.1) 29%, rgba(0,255,174,0.01) 61%, rgba(67,212,169,1) 100%);
+        }
+
+        .connector{
+            position: absolute;
+            border-top-right-radius: 30px;
+          width: 10vmin;
+          height: 8vmin;
+          background: linear-gradient(217deg, rgba(89,83,83,1) 9%, rgba(133,129,129,0.4290091036414566) 49%, rgba(101,99,99,1) 80%);
+          position: absolute;
+          top: 15vmin;
+          right: 0vmin;
+          }
+        .tube-goo{
+          position: absolute;
+          width: 100%;
+        }
+
+        .tube-goo.top{
+          background: linear-gradient(180deg, rgba(133, 224, 139, 0.922) 6%, rgba(0,255,132,0) 80%);
+          height: 2vmin;
+        }
+
+        
+        .tube-goo.bot{
+          bottom: 0;
+          background: linear-gradient(180deg, rgba(0,255,132,0) 10%, rgba(133,224,139,1) 20%);
+          height: 80%;
+        }
+
+.dropplets{
+  top:-5%;
+  right: 0;
+  position: absolute;
+  width: 2rem;
+  height: 2rem;
+  background-color: rgba(133, 224, 139, 0.922);
+  border-radius: 50%;
+  animation: drops 15s infinite linear;
+}
+
+.dropplets:nth-last-of-type(2n){
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
+.dropplets:nth-last-of-type(3n){
+  width: 2.5rem;
+  height: 2.5rem;
+}
+
+.dropplets:nth-last-of-type(2){
+right: 20%;
+animation-delay: 1s;
+}
+.dropplets:nth-last-of-type(3){
+right: 30%;
+animation-delay: 4s;
+}
+.dropplets:nth-last-of-type(3){
+right: 40%;
+animation-delay: 1.3s;
+}
+.dropplets:nth-last-of-type(4){
+right: 50%;
+animation-delay: 5s;
+}
+.dropplets:nth-last-of-type(5){
+right: 70%;
+animation-delay: 8s;
+}
+
+.dropplets:nth-last-of-type(6){
+right: 25%;
+animation-delay: 5.5s;
+}
+.dropplets:nth-last-of-type(7){
+right: 10%;
+animation-delay: 10s;
+}
+
+.dropplets:nth-last-of-type(8){
+right: 0%;
+animation-delay: 14s;
+}
+.dropplets:nth-last-of-type(9){
+right: 60%;
+animation-delay: 12s;
+}
+.dropplets:nth-last-of-type(10){
+right: 45%;
+animation-delay: 12.9s;
+}
+@keyframes drops {
+  0%{top:-5%;}
+  25%{top:40%;}
+  50%{top:60%;}
+  100%{top:100%;}
+}
+
+/* @media screen and (max-width: 970px) {
+
+  .drop-tube{
+    height: 100%;
+  }
+} */
+/* TUBE ANIMATION */
+
+
+
+
 article{
     width: 50%;
 }
@@ -89,7 +269,6 @@ font-size: 1.5vw;
   width: 100%;
   height: 10rem;
   box-sizing: contain;
-/* background-color: pink; */
 border: 1px solid green;
 padding: 0.5rem;
 margin-bottom: 1rem;
@@ -129,21 +308,21 @@ article{
 }
 
 @media screen and (max-width: 650px) {
-  padding: 0;
+  padding: 0 3rem;
   .wrap h1{
 font-size: 3.2vw;
 }
+
   article{
     width: 90%;
 }
+
 .list-container{
   width: 90%;
-
 }
 }
 
 @media screen and (max-width: 400px) {
-
 .work article, .education article{
  height: auto;
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -168,6 +168,19 @@ button{
   height: 3rem;
   color: white;
   background: #232323;
+  &:hover{
+    /* color: red; */
+    transition: 1s;
+    box-shadow:
+    0 0 3px #fff,
+    0 0 5px #fff,
+    0 0 11px #fff,
+    0 0 32px #0fa,
+    0 0 42px #0fa,
+    0 0 72px #0fa,
+    0 0 90px #0fa,
+    0 0 120px #0fa;
+  }
 }
 h2{
 text-align: center;
