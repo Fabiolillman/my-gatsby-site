71631be77778cf251cfb32f122de2b3468258130
Fabio Lillman <fabio.lillman@iths.se>
2023-02-25 14:53:16 +0100

    changed plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,10 +14,10 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [{
-    resolve: 'gatsby-plugin-styled-components',
+    resolve: 'gatsby-source-contentful',
     options: {
       "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
       "spaceId": process.env.CONTENTFUL_SPACE_ID
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp"]
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components"]
 };
