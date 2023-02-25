ff15cd6b0b9480345da5fc20401228a5a6d36987
Fabio Lillman <fabio.lillman@iths.se>
2023-02-25 15:29:58 +0100

    m

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,5 +19,5 @@ module.exports = {
       "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
       "spaceId": process.env.CONTENTFUL_SPACE_ID
     }
-  },"gatsby-plugin-styled-components" ]
+  },"gatsby-plugin-styled-components", "gatsby-plugin-image"]
 };
