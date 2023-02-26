3bdcff2f73d014324c977fa36d06875a6439ae5d
Fabio Lillman <fabio.lillman@iths.se>
2023-02-26 18:43:18 +0100

    m

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,51 +26,51 @@ exports.createPages = async({graphql, actions}) =>{
 // })
 
 // Query for projects
-const { data: projectsData } = await graphql(`
-query getAllProjectSlugs {
-  allContentfulProjects {
-    nodes {
-      slug
-    }
-  }
-}
-`)
+// const { data: projectsData } = await graphql(`
+// query getAllProjectSlugs {
+//   allContentfulProjects {
+//     nodes {
+//       slug
+//     }
+//   }
+// }
+// `)
 
 // Loop through projects and create pages
-projectsData.allContentfulProjects.nodes.forEach(node => {
-    actions.createPage({
-        //URL
-        path: '/projects/' + node.slug,
-        // Template
-        component: path.resolve('./src/templates/projects.jsx'),
-        //Content
-        context: {slug: node.slug}
-    })
-})
+// projectsData.allContentfulProjects.nodes.forEach(node => {
+//     actions.createPage({
+//         //URL
+//         path: '/projects/' + node.slug,
+//         // Template
+//         component: path.resolve('./src/templates/projects.jsx'),
+//         //Content
+//         context: {slug: node.slug}
+//     })
+// })
 
 // Query for category
-const { data: categoryData } = await graphql(`
-query getAllProjectSlugs {
-  allContentfulProjects {
-    nodes {
-      category {
-        category
-      }
-    }
-  }
-}
-`)
+// const { data: categoryData } = await graphql(`
+// query getAllProjectSlugs {
+//   allContentfulProjects {
+//     nodes {
+//       category {
+//         category
+//       }
+//     }
+//   }
+// }
+// `)
 
 // Loop through projects and create pages
-categoryData.allContentfulProjects.nodes.forEach(node => {
-    actions.createPage({
-        //URL
-        path: '/category/' + node.category.category,
-        // Template
-        component: path.resolve('./src/templates/category-list.jsx'),
-        //Content
-        context: {skill: node.category.category}
-    })
-})
+// categoryData.allContentfulProjects.nodes.forEach(node => {
+//     actions.createPage({
+//         //URL
+//         path: '/category/' + node.category.category,
+//         // Template
+//         component: path.resolve('./src/templates/category-list.jsx'),
+//         //Content
+//         context: {skill: node.category.category}
+//     })
+// })
 
 }
